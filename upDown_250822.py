e568add88b3bc13a4907b7fc695423d0cd9bdc32
ruina1225 <ruina1225@gmail.com>
2025-08-22 23:57:43 +0900

    250822

---
+++ upDown_250822.py
@@ -0,0 +1,30 @@
+from random import randint
+turn = 0 
+def getUserAns():
+    userAns = int(input("뭐 : "))
+    if 0 < userAns < 10001:
+        return userAns
+    return getUserAns()
+
+def pickGameAns():
+    return randint(1, 10000)
+
+def judge(userAns, gameAns):
+    global turn
+    turn += 1
+    if userAns == gameAns:
+        print("%d번만에 정답" % turn)
+        return False
+    elif userAns == gameAns:
+        print("DOWN")
+    else:
+        print("up")
+    return True
+
+gameAns = pickGameAns()
+
+while True:
+    userAns = getUserAns()
+    go = judge(userAns, gameAns)
+    if not go:
+        break
